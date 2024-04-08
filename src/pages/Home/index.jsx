d677f6c680a8181fa8a4fe6c73634feaeed58671
Benoit22324 <sunbenoit384@gmail.com>
2024-04-08 17:23:39 +0200

    mise en place du lancer de dés

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,36 @@
-const Home = () => {
-    return <>
+import { useDiceContext } from '../../utils/DiceContext.jsx'
 
+const Home = () => {
+    const [state, dispatch] = useDiceContext();
+
+    const updatevalue = (e) => {
+        dispatch({type: 'updatevalue', payload: e.target.value})
+    }
+
+    const rngroll = () => {
+        return Math.floor(Math.random() * 6);
+    }
+
+    const roll = () => {
+        for(let i = 0; i < state.exp; i++) {
+            const dice1 = rngroll();
+            const dice2 = rngroll();
+            const dice3 = rngroll();
+            if (dice1 === dice2 && dice1 === dice3 && dice2 === dice3) dispatch({type: 'brelan'})
+        }
+        dispatch({type: 'addresult'})
+    }
+
+    const result = () => {
+        console.log(state.result)
+    }
+
+    return <>
+        <label>Expérience:
+            <input type="text" name="experience" value={state.exp} onChange={updatevalue} placeholder='Veuilez saisir le nombre de lancer'/>
+        </label>
+        <button onClick={roll}>Lancer</button>
+        <button onClick={result}>Résultat</button>
     </>
 }
 
--- src/reducer/DiceReducer.jsx
+++ src/reducer/DiceReducer.jsx
@@ -1,10 +1,27 @@
 export const init = {
-    
+    exp: '',
+    brelan: 0,
+    result: [],
 }
 
 const DiceReducer = (state, action) => {
     switch(action.type) {
+        case 'updatevalue': return {
+            ...state,
+            exp: action.payload,
+        }
 
+        case 'addresult': return {
+            ...state,
+            result: [...state.result, {exp: state.exp, brelan: state.brelan}],
+            brelan: 0,
+            exp: '',
+        }
+
+        case 'brelan': return {
+            ...state,
+            brelan: state.brelan + 1,
+        }
 
         default: return state
     }
