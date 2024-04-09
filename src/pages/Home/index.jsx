9f21b5fac166d3903713355da381da31e5a7b80b
Benoit22324 <sunbenoit384@gmail.com>
2024-04-09 10:23:57 +0200

    Ajout du msg d'erreur et correction

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -13,18 +13,25 @@ const Home = () => {
     }
 
     const roll = () => {
-        for(let i = 0; i < state.exp; i++) {
-            const dice1 = rngroll();
-            const dice2 = rngroll();
-            const dice3 = rngroll();
-            if (dice1 === dice2 && dice1 === dice3 && dice2 === dice3) dispatch({type: 'brelan'})
+        if (state.exp.trim() !== '' && state.exp.trim() !== '0') {
+            for(let i = 0; i < state.exp; i++) {
+                const dice1 = rngroll();
+                const dice2 = rngroll();
+                const dice3 = rngroll();
+                if (dice1 === dice2 && dice1 === dice3 && dice2 === dice3) dispatch({type: 'brelan'})
+            }
+            dispatch({type: 'addresult'})
         }
-        dispatch({type: 'addresult'})
+        else if (state.exp.trim() === '') dispatch({type: 'adderr', payload: 'Veuillez saisir le nombre d\'expérience'})
+        else if (state.exp.trim() === '0') dispatch({type: 'adderr', payload: 'Veuillez saisir un chiffre supérieur à 0'})
     }
 
     return <>
         <div className='home'>
             <div className='game_form'>
+                {
+                    state.errmsg !== '' && <p className='errormsg'>{state.errmsg}</p>
+                }
                 <label>Expérience:
                     <input type="text" name="experience" value={state.exp} onChange={updatevalue} placeholder='Veuilez saisir le nombre de lancer'/>
                 </label>
--- src/reducer/DiceReducer.jsx
+++ src/reducer/DiceReducer.jsx
@@ -3,6 +3,7 @@ export const init = {
     brelan: 0,
     total: 0,
     result: [],
+    errmsg: '',
 }
 
 const DiceReducer = (state, action) => {
@@ -10,19 +11,26 @@ const DiceReducer = (state, action) => {
         case 'updatevalue': return {
             ...state,
             exp: action.payload,
+            errmsg: '',
         }
 
         case 'addresult': return {
             ...state,
             result: [...state.result, {exp: state.exp, brelan: state.brelan}],
-            total: state.brelan,
+            total: state.total + state.brelan,
             brelan: 0,
             exp: '',
+            errmsg: '',
         }
 
         case 'brelan': return {
             ...state,
             brelan: state.brelan + 1,
+        }
+
+        case 'adderr': return {
+            ...state,
+            errmsg: action.payload,
         }
 
         default: return state
